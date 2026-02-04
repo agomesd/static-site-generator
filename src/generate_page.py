9bbdfb0a3e0251c525181abe833072c004dfea98
agomesd <alex.gduguay@gmail.com>
2026-02-04 19:02:22 +0000

    Build pages properly

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,58 +1,46 @@
 import os
-from helper_funcs import markdown_to_html_node
 from pathlib import Path
+from helper_funcs import markdown_to_html_node
 
 
-class GeneratePage:
-    def __init__(self, from_path, template_path, dest_path):
-        self.from_path = from_path
-        self.template_path = template_path
-        self.dest_path = dest_path
-        self.html = None
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+    for filename in os.listdir(dir_path_content):
+        from_path = os.path.join(dir_path_content, filename)
+        dest_path = os.path.join(dest_dir_path, filename)
+        if os.path.isfile(from_path):
+            dest_path = Path(dest_path).with_suffix(".html")
+            generate_page(from_path, template_path, dest_path)
+        else:
+            generate_pages_recursive(from_path, template_path, dest_path)
 
-    def generate_pages_recursive(self, from_path=None, dest_path=None):
-        from_path = from_path or self.from_path
-        dest_path = dest_path or self.dest_path
 
-        print(
-            f"Generating pages from {from_path} to {dest_path} using {self.template_path}"
-        )
+def generate_page(from_path, template_path, dest_path):
+    print(f" * {from_path} {template_path} -> {dest_path}")
+    from_file = open(from_path, "r")
+    markdown_content = from_file.read()
+    from_file.close()
 
-        for entry in os.listdir(from_path):
-            entry_path = os.path.join(from_path, entry)
-            dest_entry_path = os.path.join(dest_path, entry)
+    template_file = open(template_path, "r")
+    template = template_file.read()
+    template_file.close()
 
-            if os.path.isfile(entry_path):
-                dest_path_html = Path(dest_entry_path).with_suffix(".html")
-                self.generate_page(entry_path, dest_path_html)
-            else:
-                os.makedirs(dest_entry_path, exist_ok=True)
-                self.generate_pages_recursive(entry_path, dest_entry_path)
+    node = markdown_to_html_node(markdown_content)
+    html = node.to_html()
 
-    def generate_page(self, from_path, dest_path):
-        print(
-            f"Generating page from {from_path} to {dest_path} using {self.template_path}"
-        )
+    title = extract_title(markdown_content)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
 
-        with open(from_path, "r", encoding="utf-8") as f:
-            markdown = f.read()
+    dest_dir_path = os.path.dirname(dest_path)
+    if dest_dir_path != "":
+        os.makedirs(dest_dir_path, exist_ok=True)
+    to_file = open(dest_path, "w")
+    to_file.write(template)
 
-        with open(self.template_path, "r", encoding="utf-8") as f:
-            template = f.read()
 
-        content = markdown_to_html_node(markdown).to_html()
-        title = self.extract_title(markdown)
-        template = template.replace("{{ Title }}", title).replace(
-            "{{ Content }}", content
-        )
-
-        path = Path(dest_path)
-        path.parent.mkdir(parents=True, exist_ok=True)
-        path.write_text(template)
-
-    def extract_title(self, markdown):
-        for line in markdown.splitlines():
-            if line.startswith("# "):
-                return line[2:].strip()
-
-        raise ValueError("no title header found")
+def extract_title(md):
+    lines = md.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("no title found")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from generate_static import generate_static
-from generate_page import GeneratePage
+from generate_page import generate_pages_recursive
+import sys
 
 BASE_DIR = Path(__file__).resolve().parent
 
 
 def main():
+    basepath = sys.argv[0] or "/"
+
     source = BASE_DIR.parent / "static"
     target = BASE_DIR.parent / "public"
     from_path = BASE_DIR.parent / "content"
@@ -13,8 +16,8 @@ def main():
     dest_path = target
     generate_static(source, target)
 
-    page_gen = GeneratePage(from_path, template, dest_path)
-    page_gen.generate_pages_recursive()
+    print("Generating content...")
+    generate_pages_recursive(from_path, template, dest_path)
 
 
 if __name__ == "__main__":
