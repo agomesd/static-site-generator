c08406b5b830e0f58f3261a096a3b3623de9a164
agomesd <alex.gduguay@gmail.com>
2026-02-09 20:10:53 +0000

    Remove basepath logic

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,20 +1,20 @@
 import os
 from pathlib import Path
-from helper_funcs import markdown_to_html_node, rewrite_paths
+from helper_funcs import markdown_to_html_node
 
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path, basepath)
+            generate_page(from_path, template_path, dest_path)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path, basepath)
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 
-def generate_page(from_path, template_path, dest_path, basepath):
+def generate_page(from_path, template_path, dest_path):
     print(f" * {from_path} {template_path} -> {dest_path}")
     from_file = open(from_path, "r")
     markdown_content = from_file.read()
@@ -26,14 +26,9 @@ def generate_page(from_path, template_path, dest_path, basepath):
 
     node = markdown_to_html_node(markdown_content)
     html = node.to_html()
-    html = rewrite_paths(html, basepath)
 
     title = extract_title(markdown_content)
-    template = (
-        template.replace("{{ Title }}", title)
-        .replace("{{ Content }}", html)
-        .replace("{{ BasePath }}", basepath)
-    )
+    template = template.replace("{{ Title }}", title).replace("{{ Content }}", html)
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
--- src/main.py
+++ src/main.py
@@ -1,13 +1,11 @@
 from pathlib import Path
 from generate_static import generate_static
 from generate_page import generate_pages_recursive
-import sys
 
 BASE_DIR = Path(__file__).resolve().parent
 
 
 def main():
-    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
     source = BASE_DIR.parent / "static"
     target = BASE_DIR.parent / "docs"
@@ -17,7 +15,7 @@ def main():
     generate_static(source, target)
 
     print("Generating content...")
-    generate_pages_recursive(from_path, template, dest_path, basepath)
+    generate_pages_recursive(from_path, template, dest_path)
 
 
 if __name__ == "__main__":
